0de33041a3c03f2fa79c48058dab6da2db0b7974
Frédéric MOHIER <frederic.mohier@gmail.com>
2018-04-17 05:42:25 +0200

    Fix a missing requirements in setup.py

--- setup.py
+++ setup.py
@@ -161,7 +161,9 @@ setup(
     data_files = data_files,
 
     # Dependencies (if some) ...
-    install_requires=[],
+    install_requires=[
+        "alignak-checks-monitoring"
+    ],
 
     # Entry points (if some) ...
     entry_points={
